e210f8f36790882061c8d8557c4c5f32ff58ee57
Ronald Dilley <rdilley@usgs.gov>
2014-03-27 20:31:34 +0000

    Added comments for possible raw_binary in the future
    git-svn-id: http://espa.googlecode.com/svn/branches/adding-lpvs@601 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- espa/browse.py
+++ espa/browse.py
@@ -15,6 +15,13 @@ History:
       file.
 '''
 
+# TODO - Currently we are not generating browse products.  And this code needs
+#        to be updated for the raw_binary format and processing.  Commented
+#        gdal commands have been added that may be the solution for various
+#        steps, or will be close to the final solution.
+#        This code should also be modified to cleanup all of it's temporary
+#        files.  The calling code should only see the product.
+
 import os
 import sys
 import glob
@@ -62,18 +69,21 @@ def do_sr_browse (sr_filename, scene, resolution=default_resolution):
                           '-ot', 'Byte',
                           '-scale', '0', '10000', '0', '255',
                           '-of', 'GTIFF']
+# gdal_translate -ot Byte -scale 0 10000 0 255 -of ENVI LT50460282002042EDC01_toa_band5.img browse_5.img
     cmd = base_translate_cmd + ['out.tiff5', 'browse.tiff5']
     cmd = ' '.join(cmd)
     log ('Running: ' + cmd)
     output = util.execut_cmd (cmd)
     log (output)
 
+# gdal_translate -ot Byte -scale 0 10000 0 255 -of ENVI LT50460282002042EDC01_toa_band4.img browse_4.img
     cmd = base_translate_cmd + ['out.tiff4', 'browse.tiff4']
     cmd = ' '.join(cmd)
     log ('Running: ' + cmd)
     output = util.execut_cmd (cmd)
     log (output)
 
+# gdal_translate -ot Byte -scale 0 10000 0 255 -of ENVI LT50460282002042EDC01_toa_band3.img browse_3.img
     cmd = base_translate_cmd + ['out.tiff3', 'browse.tiff3']
     cmd = ' '.join(cmd)
     log ('Running: ' + cmd)
@@ -82,6 +92,7 @@ def do_sr_browse (sr_filename, scene, resolution=default_resolution):
 
     #-------------------------------------------------------------------------
     # Create the 3 band composite
+# gdal_merge_simple -of ENVI -in browse_5.img -in browse_4.img -in browse_3.img -out final.img
     cmd = ['gdal_merge_simple',
            '-in', 'browse.tiff5',
            '-in', 'browse.tiff4',
@@ -94,6 +105,7 @@ def do_sr_browse (sr_filename, scene, resolution=default_resolution):
 
     #-------------------------------------------------------------------------
     # Project to geographic
+# gdalwarp -of ENVI -dstalpha -srcnodata 0 -t_srs EPSG:4326 final.img warped.img
     cmd = ['gdalwarp',
            '-dstalpha',
            '-srcnodata', '0',
@@ -106,6 +118,8 @@ def do_sr_browse (sr_filename, scene, resolution=default_resolution):
 
     #-------------------------------------------------------------------------
     # Resize and rename
+# gdal_translate -ot INT16 -of ENVI -outsize 50 50 -a_nodata -9999 warped.img browse.img
+# Should probably use gdalwarp to set the resolution, because outsize in gdal_translate is a percentage.  This step may not even be needed then, because it could be handled in the previous gdalwarp step.
     cmd = ['gdal_translate',
            '-co', 'COMPRESS=DEFLATE',
            '-co', 'PREDICTOR=2',
